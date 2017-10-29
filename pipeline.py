be0a2c208c436ab6f85de165d5d95ce70c8a8ea8
Akshat Pandey <akshat0123@gmail.com>
2017-10-29 01:46:40 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -2,8 +2,8 @@ from vectorize import vectorize
 
 def main():
 
-    DATA_PATH = '../Sample Data' # Path of directory containing 'aps' files
-    LABEL_PATH = '../Sample Data/labels.csv' # Path of csv file with labels
+    DATA_PATH = '' # Path of directory containing 'aps' files
+    LABEL_PATH = '' # Path of csv file with labels
 
     body_zone_vectors = vectorize(DATA_PATH, LABEL_PATH)
 
